8f00fb8a74f79da06c326de89d223d467b0c4bb6
drcatjk <jakobbk3@gmail.com>
2024-02-06 16:11:50 +0100

    progress on docker (it doesn't instantly crash and app.py doesnt ahve to be in same directory)

---
+++ Dockerfile
@@ -0,0 +1,28 @@
+# Use the official Python image as the base image
+FROM python:3.8-slim
+
+# Install build dependencies
+RUN apt-get update \
+    && apt-get install -y build-essential libmariadb-dev-compat pkg-config \
+    && rm -rf /var/lib/apt/lists/*
+
+# Set the working directory inside the container
+WORKDIR /app
+
+# Copy the requirements.txt file into the container at /app
+COPY requirements.txt /app/
+
+# Install Python packages
+RUN pip install --no-cache-dir -r requirements.txt
+
+# Copy the current directory contents into the container at /app
+COPY . /app/
+
+# Expose port 5001 for Flask app
+EXPOSE 5000
+
+# Copy the SQL initialization script into the container at /app
+COPY script.sql /app/
+
+# Command to run the application and initialize the database
+CMD ["sh", "-c", "python code/app.py && mysql -h localhost -u root -ppassword < /app/init.sql"]
---
+++ script.sql
@@ -0,0 +1,127 @@
+-- MySQL Workbench Forward Engineering
+
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
+
+-- -----------------------------------------------------
+-- Schema mydb
+-- -----------------------------------------------------
+
+-- -----------------------------------------------------
+-- Schema mydb
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `mydb` ;
+USE `mydb` ;
+
+-- -----------------------------------------------------
+-- Table `mydb`.`Accounts`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `mydb`.`Accounts` (
+  `e-mail` VARCHAR(320) NOT NULL,
+  `acc_name` VARCHAR(255) NOT NULL,
+  `password` VARCHAR(127) NULL,
+  `date_created` DATE NULL,
+  `admin` TINYINT(1) NULL,
+  PRIMARY KEY (`e-mail`),
+  UNIQUE INDEX `e-mail_UNIQUE` (`e-mail` ASC) VISIBLE)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `mydb`.`Products`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `mydb`.`Products` (
+  `pro_ID` VARCHAR(12) NOT NULL,
+  `pro_name` VARCHAR(255) NOT NULL,
+  `pro_img` VARCHAR(255) NULL,
+  `pro_info` VARCHAR(1023) NULL,
+  `qty` INT NOT NULL,
+  `price` INT UNSIGNED NOT NULL,
+  PRIMARY KEY (`pro_ID`),
+  UNIQUE INDEX `pro_ID_UNIQUE` (`pro_ID` ASC) VISIBLE,
+  UNIQUE INDEX `pro_name_UNIQUE` (`pro_name` ASC) VISIBLE)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `mydb`.`Reviews`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `mydb`.`Reviews` (
+  `re_ID` VARCHAR(12) NOT NULL,
+  `comment` VARCHAR(255) NULL,
+  `nr_stars` TINYINT(1) NOT NULL,
+  `date_created` DATE NULL,
+  `acc_e-mail` VARCHAR(320) NOT NULL,
+  `pro_ID` VARCHAR(12) NOT NULL,
+  PRIMARY KEY (`re_ID`),
+  UNIQUE INDEX `re_ID_UNIQUE` (`re_ID` ASC) VISIBLE,
+  INDEX `fk_Reviews_Accounts_idx` (`acc_e-mail` ASC) VISIBLE,
+  INDEX `fk_Reviews_Products1_idx` (`pro_ID` ASC) VISIBLE,
+  CONSTRAINT `fk_Reviews_Accounts`
+    FOREIGN KEY (`acc_e-mail`)
+    REFERENCES `mydb`.`Accounts` (`e-mail`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_Reviews_Products1`
+    FOREIGN KEY (`pro_ID`)
+    REFERENCES `mydb`.`Products` (`pro_ID`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `mydb`.`Balance_Changes`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `mydb`.`Balance_Changes` (
+  `change_ID` VARCHAR(12) NOT NULL,
+  `is_purchase` TINYINT(1) NOT NULL,
+  `qty` INT NOT NULL,
+  `date` DATETIME NOT NULL,
+  `acc_e-mail` VARCHAR(320) NOT NULL,
+  `pro_ID` VARCHAR(12) NOT NULL,
+  PRIMARY KEY (`change_ID`),
+  UNIQUE INDEX `change_ID_UNIQUE` (`change_ID` ASC) VISIBLE,
+  UNIQUE INDEX `date_UNIQUE` (`date` ASC) VISIBLE,
+  INDEX `fk_Balance_Changes_Accounts1_idx` (`acc_e-mail` ASC) VISIBLE,
+  INDEX `fk_Balance_Changes_Products1_idx` (`pro_ID` ASC) VISIBLE,
+  CONSTRAINT `fk_Balance_Changes_Accounts1`
+    FOREIGN KEY (`acc_e-mail`)
+    REFERENCES `mydb`.`Accounts` (`e-mail`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_Balance_Changes_Products1`
+    FOREIGN KEY (`pro_ID`)
+    REFERENCES `mydb`.`Products` (`pro_ID`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `mydb`.`Cart`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `mydb`.`Cart` (
+  `qty` INT NOT NULL,
+  `acc_e-mail` VARCHAR(320) NOT NULL,
+  `pro_ID` VARCHAR(12) NOT NULL,
+  PRIMARY KEY (`acc_e-mail`, `pro_ID`),
+  INDEX `fk_Cart_Accounts1_idx` (`acc_e-mail` ASC) VISIBLE,
+  INDEX `fk_Cart_Products1_idx` (`pro_ID` ASC) VISIBLE,
+  CONSTRAINT `fk_Cart_Accounts1`
+    FOREIGN KEY (`acc_e-mail`)
+    REFERENCES `mydb`.`Accounts` (`e-mail`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_Cart_Products1`
+    FOREIGN KEY (`pro_ID`)
+    REFERENCES `mydb`.`Products` (`pro_ID`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
